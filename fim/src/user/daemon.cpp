d6b89616cf18320d8d4ab35b02fa5da58d4761d7
Abinash Singh <abinashsinghlalotra@gmail.com>
2026-05-12 15:02:43 +0530

    USER: removed pipe threads

--- fim/src/user/daemon.cpp
+++ fim/src/user/daemon.cpp
@@ -6,23 +6,9 @@
 #include <string.h>
 #include <sys/stat.h>
 #include <syslog.h>
-#include <thread>
 #include <unistd.h>
 
-static void pipe_to_syslog(int fd, int priority) {
-  char buf[1024];
-  FILE *stream = fdopen(fd, "r");
-  if (!stream)
-    return;
-  while (fgets(buf, sizeof(buf), stream) != NULL) {
-    buf[strcspn(buf, "\n")] = 0;
-    if (strlen(buf) > 0) {
-      syslog(priority, "%s", buf);
-    }
-  }
-}
-
-void Daemon::init(const char* pid_file_path) {
+void Daemon::init(const char *pid_file_path) {
 
   /* fork and detach the parent */
   pid_t pid = fork();
@@ -58,34 +44,5 @@ void Daemon::init(const char* pid_file_path) {
     fclose(pid_file);
   }
 
-  // We don't close all FDs blindly because Daemon::init() is called
-  // at the very beginning of main(), so only standard FDs are open.
-  // Closing up to _SC_OPEN_MAX can be slow and unnecessary here.
-
   openlog("fim", LOG_PID | LOG_NDELAY, LOG_DAEMON);
-
-  int pipe_out[2], pipe_err[2];
-  if (pipe(pipe_out) == 0 && pipe(pipe_err) == 0) {
-    dup2(pipe_out[1], STDOUT_FILENO);
-    dup2(pipe_err[1], STDERR_FILENO);
-    close(pipe_out[1]);
-    close(pipe_err[1]);
-
-    // Force unbuffered output so logs appear in syslog immediately
-    // rather than being held in C library buffers until they fill up.
-    setbuf(stdout, NULL);
-    setbuf(stderr, NULL);
-
-    std::thread(pipe_to_syslog, pipe_out[0], LOG_INFO).detach();
-    std::thread(pipe_to_syslog, pipe_err[0], LOG_ERR).detach();
-  }
-
-  // Redirect standard input to /dev/null
-  int fd0 = open("/dev/null", O_RDWR);
-  if (fd0 != -1) {
-    dup2(fd0, STDIN_FILENO);
-    if (fd0 > 2) { // Just in case it's not 0, 1, or 2
-      close(fd0);
-    }
-  }
 }
